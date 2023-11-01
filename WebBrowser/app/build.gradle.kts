b6fa93034f322dff6b556cadb3e545d4dc467448
Relampago <josemariar.l17@gmail.com>
2023-11-01 15:27:13 +0100

    finished

--- WebBrowser/app/build.gradle.kts
+++ WebBrowser/app/build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.login"
+    namespace = "com.example.webbrowser"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.login"
-        minSdk = 24
-        targetSdk = 33
+        applicationId = "com.example.webbrowser"
+        minSdk = 34
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,20 +33,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.google.android.material:material:1.3.0-alpha02")
-    implementation ("com.scwang.wave:MultiWaveHeader:1.0.0")
-
-
-
-
-
-
-
-
-
-
 }
