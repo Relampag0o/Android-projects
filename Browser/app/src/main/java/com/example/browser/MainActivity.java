e07c784fdbdc7ff795fd11aff3f7493a00990dd5
Relampag0o <josemariar.l17@gmail.com>
2023-11-27 12:02:54 +0100

    .
    .

--- Browser/app/src/main/java/com/example/browser/MainActivity.java
+++ Browser/app/src/main/java/com/example/browser/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.browser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+    private void loadFragment(Fragment fragment) {
+        Log.d("FragmentTag", "Loading fragment: " + fragment.getClass().getSimpleName());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+
     }
 }
