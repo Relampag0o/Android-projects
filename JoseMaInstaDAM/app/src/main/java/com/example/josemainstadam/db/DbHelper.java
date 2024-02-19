0a451b7b788e821747cbf137d3f5d1ca97907344
Relampag0o <josemariar.l17@gmail.com>
2024-02-19 17:48:14 +0100

    .
    .

--- JoseMaInstaDAM/app/src/main/java/com/example/josemainstadam/db/DbHelper.java
+++ JoseMaInstaDAM/app/src/main/java/com/example/josemainstadam/db/DbHelper.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.security.MessageDigest;
+
 public class DbHelper extends SQLiteOpenHelper {
 
     public DbHelper(Context context) {
@@ -27,17 +29,38 @@ public class DbHelper extends SQLiteOpenHelper {
 
     public void insertBBDD(String name, String email, String pasw) {
 
+        // hashing password
+        String hashedPassword = hashPassword(pasw);
+
         // Creamos mapa de valores con los nombres de las tablas
         ContentValues values = new ContentValues();
         values.put(StructureDB.COLUMN_USERNAME, name);
         values.put(StructureDB.COLUMN_EMAIL, email);
-        values.put(StructureDB.COLUMN_PASSWORD, pasw);
+        values.put(StructureDB.COLUMN_PASSWORD, hashedPassword);
 
         // Insertar nueva fila indicando nombre de la tabla
         long newRowId = this.getWritableDatabase().insert(
                 StructureDB.TABLE_USERS, null, values);
         //this.close();
 
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 
@@ -57,10 +80,13 @@ public class DbHelper extends SQLiteOpenHelper {
 
     public boolean checkPasswd(String email, String passwd) {
 
+        String hashedPassword = hashPassword(passwd);
+
+
         Cursor cursor = this.getReadableDatabase().rawQuery(
                 "SELECT * FROM " + StructureDB.TABLE_USERS +
                         " WHERE " + StructureDB.COLUMN_EMAIL + "=? AND " + StructureDB.COLUMN_PASSWORD + "=?",
-                new String[]{email, passwd});
+                new String[]{email, hashedPassword});
 
         boolean exists = cursor.getCount() > 0;
         cursor.close();
